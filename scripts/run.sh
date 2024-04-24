ae187094b59e3b07f7276502bb358c52d4efb3cb
Benji Visser <benji@093b.org>
2024-04-24 12:38:58 -0400

    update pnpm
    Signed-off-by: Benji Visser <benji@093b.org>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Define total tasks
-# tasks=$(jq '. | length' tasks.json)
-tasks=1
+tasks=$(jq '. | length' tasks.json)
 
 # Define concurrency level
 concurrency=1
@@ -12,6 +11,8 @@ export LLM_API_KEY=$(printenv LLM_API_KEY)
 # Use seq to generate a sequence of numbers, then pipe to xargs to manage concurrency
 seq 0 $((tasks - 1)) | xargs -I {} -P $concurrency sh -c '
   id=$(jq -r ".[{}].id" tasks.json)
+  nodeVersion=$(jq -r ".[{}].nodeVersion" tasks.json)
   mkdir -p "./output/${id}"
+  . ~/.nvm/nvm.sh && nvm use $nodeVersion
   npx tsx src/eval.ts {} > "./output/${id}/output.log" 2>&1
 '
