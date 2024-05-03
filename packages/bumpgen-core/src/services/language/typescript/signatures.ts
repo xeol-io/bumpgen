dc20606596c4817505f3a803557fff8bd58140b4
Benji Visser <benji@093b.org>
2024-05-03 16:18:44 -0400

    type signature for full declaration
    Signed-off-by: Benji Visser <benji@093b.org>

--- packages/bumpgen-core/src/services/language/typescript/signatures.ts
+++ packages/bumpgen-core/src/services/language/typescript/signatures.ts
@@ -102,8 +102,23 @@ const indexSignatureDeclarationSignature = (
 };
 
 const functionDeclarationSignature = (node: FunctionDeclaration) => {
-  const typeDef = node.getText();
-  return enrichWithTypeReferences(typeDef, node);
+  // check if the node is a type decalaration or the full implementation
+  if (node.getDescendantsOfKind(SyntaxKind.Block).length >= 1) {
+    // ref: https://github.com/dsherret/ts-morph/issues/907
+    const params = node
+      .getParameters()
+      .map((parameter) => parameter.getText())
+      .join(", ");
+    const returnType = node.getReturnType().getText(
+      node,
+      // https://github.com/dsherret/ts-morph/issues/453#issuecomment-667578386
+      TypeFormatFlags.UseAliasDefinedOutsideCurrentScope,
+    );
+    return enrichWithTypeReferences(`(${params}) => ${returnType}`, node);
+  } else {
+    const typeDef = node.getText();
+    return enrichWithTypeReferences(typeDef, node);
+  }
 };
 
 const variableDeclarationSignature = (node: VariableDeclaration) => {
@@ -112,8 +127,19 @@ const variableDeclarationSignature = (node: VariableDeclaration) => {
 };
 
 const methodDeclarationSignature = (node: MethodDeclaration) => {
-  const typeDef = node.getText();
-  return enrichWithTypeReferences(typeDef, node);
+  // check if the node is a type decalaration or the full implementation
+  if (node.getDescendantsOfKind(SyntaxKind.Block).length >= 1) {
+    // ref: https://github.com/dsherret/ts-morph/issues/907
+    const params = node
+      .getParameters()
+      .map((parameter) => parameter.getText())
+      .join(", ");
+    const returnType = node.getReturnType().getText();
+    return enrichWithTypeReferences(`(${params}) => ${returnType}`, node);
+  } else {
+    const typeDef = node.getText();
+    return enrichWithTypeReferences(typeDef, node);
+  }
 };
 
 const heritageClauseSignature = (node: HeritageClause) => {
